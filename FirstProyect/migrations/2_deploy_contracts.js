6a237e3c3a830c4384e350c06dd6e01419ef75fb
victorma17 <victorma17@hotmail.com>
2023-02-09 13:24:56 +0100

    Supply Chain app

--- FirstProyect/migrations/2_deploy_contracts.js
+++ FirstProyect/migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 const HelloWorld = artifacts.require("HelloWorld");
+// const DataTypes = artifacts.require("DataTypes");
 
 module.exports = function (deployer) {
     deployer.deploy(HelloWorld);
 }
+/*
+module.exports = function (deployer) {
+    deployer.deploy(DataTypes);
+}
+*/
